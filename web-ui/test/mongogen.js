c5daadb0e36c31f1507765379bc0b312e8fd3393
luk <jaroszewski.luk@gmail.com>
2015-11-20 19:23:14 +0100

    mongodb test data generator update

--- web-ui/test/mongogen.js
+++ web-ui/test/mongogen.js
@@ -9,25 +9,18 @@ load("../assets/node_modules/chance/chance.js");
 load("../assets/node_modules/moment/moment.js");
 
 
-var db = new Mongo().getDB("testdb1");
-var col = db.getCollectionNames();
 
 print('Script Mongo test start');
 
 
+var sets = Array.apply(0, Array(3)).map(function(x,y){
+  var period = [
+    {duration: {h:2}, work_mode:'work' },
+    {duration: {h:2}, work_mode:'work' }
+  ];
+  return period;
+});
 
-var period = [
-  {duration: {h:2}, work_mode:'work' },
-  {duration: {h:2}, work_mode:'work' }
-];
-
-var day2 = [
-  '2h prod active',
-  '3h prod idle',
-  '1-3h '
-
-
-];
 
 
 
@@ -87,60 +80,123 @@ var date = moment();
 //printjson(all_workers);
 
 
+var events = [];
 
 var probe_time_start = moment().startOf('day');
 
+sets.map(function (xxx, yyy){
 
-period.map(function (xx, yy) {
-
-  var worker = chance.pick(workers);
-  var leader_name =  all_workers.filter(function(x){return x._id === worker.leader})[0].fullname;
-
-  var template =  {
-    "_id" : 'ObjectId(' +chance.guid()+')',
-    "worker_id" : worker._id,
-    "duration" : 3600,
-    "app_category" : "productive",
-
-    "leader_name" : leader_name,
-
-    "print_qty" : 3,
-    "total_downloads_size" : 2145,
-    "workstation" : chance.country({ full: true }).toUpperCase(),
-    "effectiveness" : 100,
-    "probe_time" : '------',
-
-    "sample" : {
-      "image_fs_name" : "app2.exe",
-      "resource_image_name" : "",
-      "window_caption" : "",
-      "image_full_path" : ""
-    },
-
-    "user" : {
-      "presence" : "active",
-      "work_mode" : "work",
-      "user_sid" : 'ObjectId(' +chance.guid()+')', // probably windows user sid
-      "user_login" : worker.login
-    },
-  };
-
-  var tmp = template;//JSON.parse(JSON.stringify(template));
+  print('Generate events for person '+yyy);
 
 
-  Array.apply(0, Array(moment.duration(xx.duration).asSeconds())).map(function (x, y) {
-    var probe_time = probe_time_start.clone();
-    probe_time.add(y, 'seconds');
-    tmp.probe_time = probe_time.local().toISOString();
-    tmp.duration = 1;
+  xxx.map(function (xx, yy) {
+    print('    period '+yy);
 
-    tmp.user.work_mode = xx.work_mode;
-    tmp.sample.image_fs_name = chance.pick(['gg','chrome','iexplorer','gra5','mediaplayer','app1'])+'.exe';
+    var worker = chance.pick(workers);
+    var leader_name =  all_workers.filter(function(x){return x._id === worker.leader})[0].fullname;
 
+    var template =  {
+      "_id" : 'ObjectId(' +chance.guid()+')',
+      "worker_id" : worker._id,
+      "duration" : 3600,
+      "app_category" : "productive",
 
-    //printjson(y+'   '+moment(tmp.probe_time).toLocaleString());
+      "leader_name" : leader_name,
+
+      "print_qty" : 3,
+      "total_downloads_size" : 2145,
+      "workstation" : chance.country({ full: true }).toUpperCase(),
+      "effectiveness" : 100,
+      "probe_time" : '------',
+
+      "sample" : {
+        "image_fs_name" : "app2.exe",
+        "resource_image_name" : "",
+        "window_caption" : "",
+        "image_full_path" : ""
+      },
+
+      "user" : {
+        "presence" : "active",
+        "work_mode" : "work",
+        "user_sid" : 'ObjectId(' +chance.guid()+')', // probably windows user sid
+        "user_login" : worker.login
+      },
+    };
+
+    var tmp = template;//JSON.parse(JSON.stringify(template));
+
+    var event_n = moment.duration(xx.duration).asSeconds();
+
+    Array.apply(0, Array(event_n)).map(function (x, y) {
+
+      tmp._id = 'ObjectId(' +chance.guid()+')';
+      var probe_time = probe_time_start.clone();
+      probe_time.add(y, 'seconds');
+      tmp.probe_time = probe_time.local().toISOString();
+      tmp.duration = 1;
+
+      tmp.user.work_mode = xx.work_mode;
+      tmp.sample.image_fs_name = chance.pick(['gg','chrome','iexplorer','gra5','mediaplayer','app1'])+'.exe';
+
+      events.push(tmp);
+    });
+
+    print('        event 0..'+event_n);
+
   });
 
-  printjson(tmp);
 });
 
+print('>>>> Rotate testdb1');
+
+var dbname = 'testdb1';
+var cleardb = true;
+
+var conn = new Mongo();
+var db = conn.getDB(dbname);
+
+var dblist = db.adminCommand("listDatabases");
+
+var db_count_max = 0;
+
+for(var x in dblist.databases){
+  //print(dblist.databases[x].name);
+
+  var res = /(testdb)([0-9]+)/.exec(dblist.databases[x].name);
+
+  if(res !== null){
+    db_count_max = Math.max(db_count_max, res[2]);
+  }
+}
+
+print('copying '+dbname+' to '+'testdb'+(Number(db_count_max)+1));
+db.copyDatabase(dbname, 'testdb'+(Number(db_count_max)+1));
+
+db = conn.getDB(dbname);
+
+if(cleardb){
+  db.getCollectionNames().forEach(function (name) {
+
+    if( /system/.exec(name) === null)
+    {
+      if(db.getCollection(name).drop()){
+        print('Dropped collection '+name);
+      }else{
+        print('Error droping collection '+name);
+      }
+    }
+  })
+
+}
+
+print('>>>> Populating database');
+
+db.groups.insert(groups);
+db.workers.insert(all_workers);
+
+print('Events to push '+events.length);
+
+print('Result inserted '+db.events.insert(events).nInserted);
+
+print('Events in collection '+db.events.count());
