79b40c77be82427e246d571e29701edabfd098e6
luk <jaroszewski.luk@gmail.com>
2016-02-22 22:19:34 +0100

    rabbitmq service added yet another lost calendar mode update (no selections)

---
+++ web-ui/api/services/EventsPersistancyService.js
@@ -0,0 +1,11 @@
+/**
+ * Created by luk on 2016-02-22.
+ */
+
+var EventsPersistancyService = {
+    accept : function acceptService(){
+        console.log('msg accepted');
+    }
+}
+
+module.exports = EventsPersistancyService;
