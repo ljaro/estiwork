7c818e579e149aa1172dbc44dff7107dc424f381
luk <jaroszewski.luk@gmail.com>
2016-02-24 21:41:28 +0100

    rabbitmq saving messages in db

--- web-ui/api/models/Event.js
+++ web-ui/api/models/Event.js
@@ -2,10 +2,13 @@
 
 
 module.exports = {
-	
-	
+
+
 	attributes:{
-		
-		
+		probe_time:{type:'datetime'},
+    duration:{type:'integer'},
+    user:{type:'json'},
+    machine:{type:'json'},
+    sample:{type:'json'}
 	}
 }
