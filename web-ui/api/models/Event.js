6d30c2fba46c735d14e841c81ddcee12c288557f
luk <jaroszewski.luk@gmail.com>
2016-03-05 15:18:00 +0100

    rabbitmq saving messages in db

--- web-ui/api/models/Event.js
+++ web-ui/api/models/Event.js
@@ -5,10 +5,20 @@ module.exports = {
 
 
 	attributes:{
+
+    /**
+     * this are original message from fields
+     */
 		probe_time:{type:'datetime'},
     duration:{type:'integer'},
     user:{type:'json'},
     machine:{type:'json'},
-    sample:{type:'json'}
+    sample:{type:'json'},
+
+    /**
+     * This are helper calculated fields
+     */
+    app_category:{type:'string'},
+    worker_id:{model:'worker'}
 	}
 }
--- web-ui/api/services/EventsPersistancyService.js
+++ web-ui/api/services/EventsPersistancyService.js
@@ -2,14 +2,57 @@
  * Created by luk on 2016-02-22.
  */
 
+var q = require('q');
+
 var EventsPersistancyService = {
-    accept : function acceptService(msg){
-      var content = msg.content.toString();
+  accept: function acceptService(msg) {
 
-      Event.create(JSON.parse(content)).then(function(x){
-
-      });
+    function handleNotFoundWorker() {
+      console.log('handleNotFoundWorker');
     }
+
+    try {
+
+      function _getAppCategory() {
+        var defer = q.defer();
+
+        setTimeout(function () {
+          defer.resolve('cos');
+        }, 500);
+
+        return defer.promise;
+      }
+
+      //TODO extend finding by more properties
+      function _getWorkerId(login) {
+        return Worker.findOneByLogin(login);
+      }
+
+      var content = msg.content.toString();
+      content = JSON.parse(content);
+
+      _getWorkerId('Tate2').then(function (res) {
+        if(res === undefined){
+          handleNotFoundWorker();
+        }
+
+
+      }).catch(function (err) {
+        console.log(err);
+      });
+
+      //_getAppCategory().when(function(err, res){
+      //  content["app_category"] = res;
+      //}).then(function(err, res){
+      //  content["worker_id"] = _getWorkerId();
+      //  Event.create(content).then(function (x) {});
+      //});
+    }
+    catch(err){
+      console.log(err);
+    }
+
+  }
 }
 
 module.exports = EventsPersistancyService;
