651ecec9c2b2f2659a7be664459eadf7d5a398f9
luk <jaroszewski.luk@gmail.com>
2016-03-12 12:07:36 +0100

    api/controllers fix

---
+++ web-ui/assets/js/src/components/blinky/blinky.js
@@ -0,0 +1,4 @@
+/**
+ * Created by luk on 2016-03-10.
+ */
+angular.module('myApp.blinky', []);
--- web-ui/assets/js/src/quickview/quickviewController.js
+++ web-ui/assets/js/src/quickview/quickviewController.js
@@ -1,84 +1,96 @@
 /**
  *
  */
-angular.module('myApp.quickview', ['ngResource', 'ui.grid','ui.bootstrap'])
+angular.module('myApp.quickview', ['ngResource', 'ui.grid', 'ui.bootstrap', 'myApp.blinky'])
 
   .controller('quickViewController',
-  ['$scope', '$http', '$resource', 'checklistSelected', 'GroupsService', 'uiGridConstants', function ($scope, $http, $resource, checklistSelected, GroupsService, uiGridConstants){
+  ['$scope', '$http', '$resource', '$interval', 'checklistSelected', 'GroupsService', 'uiGridConstants',
+    function ($scope, $http, $resource, $interval, checklistSelected, GroupsService, uiGridConstants) {
 
 
-    $scope.flatGroups = [];
+      $scope.flatGroups = [];
+      $scope.test = 0;
 
-    GroupsService.all.query(function(data) {
+      GroupsService.all.query(function (data) {
 
-      // translate groups to flat json
-      var flatGroups = [];
-      angular.forEach(data, function(value, key){
-        var newGrpItm = {'id'  : value.id, 'name': value.name, 'ticked': false};
-        this.push(newGrpItm);
-      }, flatGroups);
+        // translate groups to flat json
+        var flatGroups = [];
+        angular.forEach(data, function (value, key) {
+          var newGrpItm = {'id': value.id, 'name': value.name, 'ticked': false};
+          this.push(newGrpItm);
+        }, flatGroups);
 
-      $scope.flatGroups = flatGroups;
-      $scope.groups = data.groups;
-    });
+        $scope.flatGroups = flatGroups;
+        $scope.groups = data.groups;
+      });
 
-    $scope.groupSelections = [];
-    $scope.getSelected = function(data) {
-      console.log($scope.groupSelections);
-    };
-
-
+      $scope.groupSelections = [];
+      $scope.getSelected = function (data) {
+        console.log($scope.groupSelections);
+      };
 
 
-    $scope.loadData = function(array_of_groups) {
+      var last_groups;
 
-      console.log("load data:"+array_of_groups);
 
-      var true_array = [];
-
-      angular.forEach(array_of_groups, function(value, key){
-        console.log(value.id);
-        this.push(value.id);
-
-      }, true_array);
-
-      if(typeof true_array === 'undefined' || true_array.length == 0) {
-        $scope.tabledata = {};
-        $scope.tabledata.groups = null;
-        return;
+      function refresh() {
+        $interval(function () {
+          if (last_groups !== undefined) {
+            $scope.loadData(last_groups);
+          }
+        }, 2000, false);
       }
 
-      var Groups = $resource('/quickview/group/:id');
-      Groups.query({id:true_array}, function(data) {
-        $scope.tabledata = {groups:[]};
-        $scope.tabledata.groups = data;
-      });
-    }
+
+      $scope.loadData = function (array_of_groups) {
+
+        console.log("load data:" + array_of_groups);
+
+        last_groups = array_of_groups;
+
+        var true_array = [];
+
+        angular.forEach(array_of_groups, function (value, key) {
+          console.log(value.id);
+          this.push(value.id);
+
+        }, true_array);
+
+        if (typeof true_array === 'undefined' || true_array.length == 0) {
+          $scope.tabledata = {};
+          $scope.tabledata.groups = null;
+          return;
+        }
+
+        var Groups = $resource('/quickview/group/:id');
+        Groups.query({id: true_array}, function (data) {
+          $scope.tabledata['groups'] = data;
+        });
+      }
 
 
+      $scope.loadData($scope.groupSelections);
+      refresh();
 
 
+      $scope.test = "To jest test";
+
+      $scope.unselectGroup = function (grpId) {
+        console.log('unselecting:' + grpId);
 
 
-    $scope.loadData($scope.groupSelections);
-    $scope.test = "To jest test";
+        $scope.tabledata.groups = $scope.tabledata.groups.filter(function (obj) {
+          return obj.id !== grpId;
+        });
 
-    $scope.unselectGroup = function(grpId) {
-      console.log('unselecting:'+grpId);
+        $scope.groupSelections.splice($scope.groupSelections.indexOf(grpId), 1);
 
-
-      $scope.tabledata.groups = $scope.tabledata.groups.filter(function( obj ) {
-        return obj.id !== grpId;
-      });
-
-      $scope.groupSelections.splice($scope.groupSelections.indexOf(grpId), 1);
-
-      console.log('unselected after:'+$scope.groupSelections);
-    }
+        console.log('unselected after:' + $scope.groupSelections);
+      }
 
 
-    $scope.prepareTooltip = function(row) {
-      console.log(row);
-    }
-  }]);
+      $scope.prepareTooltip = function (row) {
+        console.log(row);
+      }
+    }]);
 
