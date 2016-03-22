c3825f68b66745bde49757809532867aed9cf3e6
luk <jaroszewski.luk@gmail.com>
2016-03-22 20:12:10 +0100

    quickview refactoring

--- web-ui/assets/js/src/quickview/quickview.js
+++ web-ui/assets/js/src/quickview/quickview.js
@@ -1,2 +1,14 @@
 
-angular.module('myApp.quickview', ['ngResource']);
+angular.module('myApp.quickview', ['ngResource', 'ui.grid', 'ui.bootstrap', 'myApp.blinky'])
+.service('Utils', function () {
+
+  this.flatGroups = function (groups) {
+    // translate groups to flat json
+    var flatGroups = [];
+    angular.forEach(groups, function (value, key) {
+      var newGrpItm = {'id': value.id, 'name': value.name, 'ticked': true};
+      this.push(newGrpItm);
+    }, flatGroups);
+    return flatGroups;
+  }
+});
