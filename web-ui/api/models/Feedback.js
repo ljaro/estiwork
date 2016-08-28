4c5203b2c975ea7370d2c711b2a21ee47b4f4a08
zironka81 <zironka81@yahoo.com>
2016-08-28 23:54:00 +0300

    button disabled for antiflood

---
+++ web-ui/api/models/Feedback.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) Łukasz Jaroszewski, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+
+module.exports = {
+	
+		
+	attributes:{
+		text: {type: 'string'}
+	}
+}
