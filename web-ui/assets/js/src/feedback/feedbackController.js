f0f8c18e0bc79b6930abe0963d69ee1efb33378d
zironka81 <zironka81@yahoo.com>
2016-08-30 20:54:13 +0300

    POST for feedback in UI

--- web-ui/assets/js/src/feedback/feedbackController.js
+++ web-ui/assets/js/src/feedback/feedbackController.js
@@ -6,16 +6,25 @@
 
 angular.module('myApp.feedback', ['ngResource'])
   .controller('feedbackController',
-  ['$scope', '$resource', '$timeout', function ($scope, $resource, $timeout){
+  ['$scope', '$resource', '$timeout', '$http', function ($scope, $resource, $timeout, $http){
   	
   	$scope.isClicked = false;
 
   	$scope.submitFeedback = function(){
   		if ($scope.isClicked === false){
   			$scope.isClicked = true;
+        var data = {text : $scope.feedback};
+        $http.post('/feedback', data)
+            .success(function (data, status, headers, config) {
+                $scope.message = data;
+            })
+            .error(function (data, status, header, config) {
+                alert( "failure message: " + JSON.stringify({data: data}));
+            });  
   			$timeout(function() {
   				$scope.isClicked = false;
 		    }, 5000);
+        $scope.feedback = "";
   		}
   	};
 
