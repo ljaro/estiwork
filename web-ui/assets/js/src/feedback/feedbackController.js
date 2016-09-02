61146a7b611d8a4f45596a1ff83d037d4abe7a22
Tanya Skutar <zironka81@users.noreply.github.com>
2016-09-02 23:27:10 +0300

    Feedback text (#23)
    * button disabled for antiflood
    
    * feedback button once again
    
    * post with feedback
    
    * POST for feedback in UI
    
    * Added timeout on server side
    
    * validation on frontend, changed server controller
    
    * feedback trimmed to 1000 on backend
    
    * model defined in the controller
    
    * comments deleted

--- web-ui/assets/js/src/feedback/feedbackController.js
+++ web-ui/assets/js/src/feedback/feedbackController.js
@@ -8,24 +8,29 @@ angular.module('myApp.feedback', ['ngResource'])
   .controller('feedbackController',
   ['$scope', '$resource', '$timeout', '$http', function ($scope, $resource, $timeout, $http){
   	
+    $scope.feedback = {};
   	$scope.isClicked = false;
 
   	$scope.submitFeedback = function(){
-      var trimmedText = $scope.feedback.trim();
-      if (trimmedText.length > 0 && $scope.isClicked === false) {
-  			$scope.isClicked = true;
-        var data = {text : trimmedText};
-        $http.post('/feedback', data)
-            .success(function (data, status, headers, confarig) {
-                console.log( "Success: " + JSON.stringify({data: data}));
-            })
-            .error(function (data, status, header, config) {
-                console.log( "failure message: " + JSON.stringify({data: data}));
-            });  
-  			$timeout(function() {
-  				$scope.isClicked = false;
-		    }, 5000);
-        $scope.feedback = "";
+
+      if (Object.keys($scope.feedback).length !== 0) {
+        var trimmedText = $scope.feedback.text.trim();
+        if (trimmedText.length > 0 && $scope.isClicked === false) {
+    			$scope.isClicked = true;
+          var data = {text : trimmedText};
+          $http.post('/feedback', data)
+              .success(function (data, status, headers, confarig) {
+                  console.log( "Success: " + JSON.stringify({data: data}));
+              })
+              .error(function (data, status, header, config) {
+                  console.log( "failure message: " + JSON.stringify({data: data}));
+              });  
+    			$timeout(function() {
+    				$scope.isClicked = false;
+  		    }, 5000);
+          $scope.feedback = {};
+        }
+
       } 
   	};
 
