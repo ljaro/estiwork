3d0a5931fea1cf6914ca3eb8a16d546bca7bd43f
zironka81 <zironka81@yahoo.com>
2016-09-19 10:09:16 +0300

    moved cats to json, added promise to q1

--- web-ui/api/services/DefaultDocsService.js
+++ web-ui/api/services/DefaultDocsService.js
@@ -5,10 +5,10 @@
  */
 
 var Q = require('q');
+var testSignatures = require('../../test/test_signatures.json');
 
 var DefaultDocsService = {
   initializeDatabase: function initializeDatabaseService() {
-
 
     console.log('init database');
     var q1 =
@@ -36,215 +36,24 @@ var DefaultDocsService = {
         })
       ];
 
-    this.populateAppSignatures();
+    var appsPromise = this.populateAppSignatures();
+    q1.push(appsPromise);
 
     return Q.all(q1);
 
   },
 
-  populateAppSignatures: function populateAppSignatures() {
-    var cats = [
-      { "name" : "Chrome",
-        "group" : "Web browser",
-        "type" : "PRODUCTIVE",
-        "signatures" : [
-          {
-            "name" : "hash",
-            "weight" : 100.0000000000000000,
-            "hash" : [
-              "056781731eaf223c799bebe04c60353ea73d60cb"
-            ],
-            "func" : "strContains"
-          },
-          {
-            "name" : "resource_image_name",
-            "weight" : 7.0000000000000000,
-            "func" : "strEquals",
-            "value": "chrome.exe"
-          },
-          {
-            "name" : "window_caption",
-            "weight" : 5.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "- Google Chrome"
-          },
-          {
-            "name" : "image_fs_name",
-            "weight" : 2.0000000000000000,
-            "func" : "strEquals",
-            "value": "chrome.exe"
-          },
-          {
-            "name" : "image_full_path",
-            "weight" : 2.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "chrome.exe"
-          }
-        ]
-      }, 
-      {
-        "name" : "Firefox",
-        "group" : "Web browser",
-        "type" : "NONPRODUCTIVE",
-        "signatures" : [
-          {
-            "name" : "hash",
-            "weight" : 100.0000000000000000,
-            "hash" : [
-              "10e596c10b5364d13a8b89a68f0ee14a27626554"
-            ],
-            "func" : "strContains"          },
-          {
-            "name" : "resource_image_name",
-            "weight" : 7.0000000000000000,
-            "func" : "strEquals",
-            "value": "firefox.exe"
-          },
-          {
-            "name" : "window_caption",
-            "weight" : 5.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "- Mozilla Firefox"
-          },
-          {
-            "name" : "image_fs_name",
-            "weight" : 2.0000000000000000,
-            "func" : "strEquals",
-            "value": "firefox.exe"
-          },
-          {
-            "name" : "image_full_path",
-            "weight" : 2.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "firefox.exe"
-          }
-        ]
-      },
-      {
-        "name" : "RabbitMQ Management",
-        "group" : "Web browser",
-        "type" : "PRODUCTIVE",
-        "signatures" : [
-          {
-            "name" : "hash",
-            "weight" : 100.0000000000000000,
-            "hash" : [
-              "10e596c10b5364d13a8b89a68f0ee14a27626554"
-            ],
-            "func" : "strContains"
-          },
-          {
-            "name" : "resource_image_name",
-            "weight" : 7.0000000000000000,
-            "func" : "strEquals",
-            "value": "firefox.exe"
-          },
-          {
-            "name" : "window_caption",
-            "weight" : 8.0000000000000000,
-            "func" : "returnTest",
-            "value": ".*RabbitMQ Management - Mozilla Firefox"             
-          },
-          {
-            "name" : "image_fs_name",
-            "weight" : 2.0000000000000000,
-            "func" : "strEquals",
-            "value": "firefox.exe"
-          },
-          {
-            "name" : "image_full_path",
-            "weight" : 2.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "firefox.exe"            
-          }
-        ]
-      },
-      {
-        "name" : "Opera",
-        "group" : "Web browser",
-        "type" : "NONPRODUCTIVE",
-        "signatures" : [
-          {
-            "name" : "hash",
-            "weight" : 100.0000000000000000,
-            "hash" : [
-              "333333",
-              "33333"
-            ],
-            "func" : "strContains"
-          },
-          {
-            "name" : "resource_image_name",
-            "weight" : 7.0000000000000000,
-            "func" : "strEquals",
-            "value": "opera.exe"
-          },
-          {
-            "name" : "window_caption",
-            "weight" : 5.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "- Opera"            
-          },
-          {
-            "name" : "image_fs_name",
-            "weight" : 2.0000000000000000,
-            "func" : "strEquals",
-            "value": "opera.exe"
-          },
-          {
-            "name" : "image_full_path",
-            "weight" : 2.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "opera.exe"            
-          }
-        ]
-      },
-      {
-        "name" : "Prod. app on Opera",
-        "group" : "Work apps",
-        "type" : "PRODUCTIVE",
-        "signatures" : [
-          {
-            "name" : "hash",
-            "weight" : 100.0000000000000000,
-            "hash" : [
-              "333333",
-              "33333"
-            ],
-            "func" : "strContains"
-          },
-          {
-            "name" : "resource_image_name",
-            "weight" : 7.0000000000000000,
-            "func" : "strEquals",
-            "value": "opera.exe"
-          },
-          {
-            "name" : "window_caption",
-            "weight" : 8.0000000000000000,
-            "func" : "returnTest",
-            "value": ".*?Prod. app on Opera - Opera"   
-          },
-          {
-            "name" : "image_fs_name",
-            "weight" : 2.0000000000000000,
-            "func" : "strEquals",
-            "value": "opera.exe"
-          },
-          {
-            "name" : "image_full_path",
-            "weight" : 2.0000000000000000,
-            "func" : "strEndsWith",
-            "value": "opera.exe"               
-          }
-        ]
+  populateAppSignatures: function populateAppSignatures($resource) {
+
+    var cats = testSignatures;
+    
+    return Q.promise(function(resolve, reject){
+      for (i = 0; i < cats.length; i++) {
+        Apps.findOrCreate(cats[i]).exec(function createFindCB(error, createdOrFoundRecords){
+  //        console.log('Not added '+createdOrFoundRecords.name+'!');
+        });
       }
-    ];
-    for (i = 0; i < cats.length; i++) {
-      Apps.findOrCreate(cats[i]).exec(function createFindCB(error, createdOrFoundRecords){
-//        console.log('Not added '+createdOrFoundRecords.name+'!');
-      });
-    }
+    });
 
   }
 
