8de66dd40faf16a4f02bfe43c7a3764e51267607
viktoriya <viktoriya.gorgul@gmail.com>
2016-09-26 13:39:55 +0300

    modified nodesSim.js

--- tools/nodesSim.js
+++ tools/nodesSim.js
@@ -1,5 +1,5 @@
 var moment = require('moment');
-var chance = require('chance').Chance(1234);
+var chance = require('chance').Chance(Math.random);
 
 /***********************************************************
  ***********************************************************
