d72caabfb6fe081d0decd04a2036684a7068285c
viktoriya <viktoriya.gorgul@gmail.com>
2016-10-13 23:48:54 +0300

    fire timeout

--- web-ui/assets/js/src/quickview/quickviewController.js
+++ web-ui/assets/js/src/quickview/quickviewController.js
@@ -6,58 +6,65 @@
 
 angular.module('myApp.quickview')
 
-  .controller('quickViewController',
-    ['$scope', '$resource', '$interval', 'GroupsService', 'Utils',
-      function ($scope, $resource, $interval, GroupsService, Utils) {
+.controller('quickViewController', ['$scope', '$resource', '$interval', 'GroupsService', 'Utils', '$timeout',
+    function($scope, $resource, $interval, GroupsService, Utils, $timeout) {
 
         $scope.groupSelections = [];
         $scope.flatGroups = [];
 
-        $scope.filterSelection = function (x) {
-          var res = $scope.flatGroups.find(function (xx) {
-            return xx.id === x && xx.ticked;
-          });
-          return res !== undefined;
+        $scope.filterSelection = function(x) {
+            var res = $scope.flatGroups.find(function(xx) {
+                return xx.id === x && xx.ticked;
+            });
+            return res !== undefined;
         }
 
-        GroupsService.all.query(function (data) {
-          $scope.flatGroups = Utils.flatGroups(data);
+        GroupsService.all.query(function(data) {
+            $scope.flatGroups = Utils.flatGroups(data);
         });
-        
-        function startPulling() {
-          $interval(function () {
+
+        var isRun = true;
+
+        $scope.$on('$destroy', function() {
+            isRun = false;
+        });
+
+    
+        $scope.loadData = function(groups) {
+
+            if (typeof groups === 'undefined') {
+                $scope.groups = null;
+                return;
+            }
+
+            var groupsIds = groups.map(function(x) {
+                return x.id;
+            });
+
+            if (typeof groupsIds === 'undefined' || groupsIds.length == 0) {
+                $scope.groups = null;
+                return;
+            }
+            if(isRun){
+                $resource('/quickview/group/:id').query({ id: groupsIds },
+                function(result) {
+                    $scope.groups = result;
+                    $scope.loadData($scope.groupSelections);
+                });
+            }
+            
+
+        }
+
+        $timeout(function() {
             $scope.loadData($scope.groupSelections);
-          }, 2000, false);
+        }, 2000);
+
+        $scope.unselectGroup = function(grpId) {
+            $scope.flatGroups.find(function(x) {
+                return x.id === grpId;
+            })['ticked'] = false;
         }
 
-        $scope.loadData = function (groups) {
-
-          if (typeof groups === 'undefined') {
-            $scope.groups = null;
-            return;
-          }
-
-          var groupsIds = groups.map(function (x) {
-            return x.id;
-          });
-
-          if (typeof groupsIds === 'undefined' || groupsIds.length == 0) {
-            $scope.groups = null;
-            return;
-          }
-
-          $resource('/quickview/group/:id').query({id: groupsIds}, function (result) {
-            $scope.groups = result;
-          });
-        }
-
-        startPulling();
-
-        $scope.unselectGroup = function (grpId) {
-          $scope.flatGroups.find(function (x) {
-            return x.id === grpId;
-          })['ticked'] = false;
-        }
-
-      }]);
-
+    }
+]);
