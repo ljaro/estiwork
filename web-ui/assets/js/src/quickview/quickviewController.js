d853c6b0db2af652ea3c2c79453c85f9e3547839
luk <evolveinvader+e1@gmail.com>
2016-11-05 15:09:04 +0100

    formatting

--- web-ui/assets/js/src/quickview/quickviewController.js
+++ web-ui/assets/js/src/quickview/quickviewController.js
@@ -6,74 +6,74 @@
 
 angular.module('myApp.quickview')
 
-.controller('quickViewController', ['$scope', '$resource', '$interval', 'GroupsService', 'Utils', '$timeout',
-    function($scope, $resource, $interval, GroupsService, Utils, $timeout) {
+  .controller('quickViewController', ['$scope', '$resource', 'GroupsService', 'Utils', '$timeout',
+    function ($scope, $resource, GroupsService, Utils, $timeout) {
 
-        $scope.groupSelections = [];
-        $scope.flatGroups = [];
+      $scope.groupSelections = [];
+      $scope.flatGroups = [];
 
-        $scope.filterSelection = function (x) {
-          var res = $scope.flatGroups.find(function (xx) {
-            return xx.id === x && xx.ticked;
-          });
-          return res !== undefined;
+      $scope.filterSelection = function (x) {
+        var res = $scope.flatGroups.find(function (xx) {
+          return xx.id === x && xx.ticked;
+        });
+        return res !== undefined;
+      }
+
+      GroupsService.all.query(function (data) {
+        $scope.flatGroups = Utils.flatGroups(data);
+      });
+
+
+      $scope.$watch('groupSelections', function (newValue, oldValue) {
+        if (typeof $scope.groups !== 'undefined' && oldValue.length == 0) {
+          $scope.loadData($scope.groupSelections);
+        }
+      });
+
+
+      $scope.loadData = function (groups, callback) {
+
+        if (typeof groups === 'undefined') {
+          $scope.groups = null;
+          return;
         }
 
-        GroupsService.all.query(function (data) {
-          $scope.flatGroups = Utils.flatGroups(data);
+        var groupsIds = groups.map(function (x) {
+          return x.id;
         });
 
+        if (typeof groupsIds === 'undefined' || groupsIds.length == 0) {
+          $scope.groups = null;
+          return;
+        }
 
-        $scope.$watch('groupSelections', function(newValue, oldValue) {
-            if (typeof $scope.groups !== 'undefined' && oldValue.length == 0) {
-                $scope.loadData($scope.groupSelections);
+        $resource('/quickview/group/:id').query({id: groupsIds},
+          function (result) {
+            $scope.groups = result;
+
+            if (typeof callback == typeof Function) {
+              callback();
             }
+          });
+      }
+
+      var startPulling = function () {
+        var callTimer = $timeout(function () {
+          $scope.loadData($scope.groupSelections, startPulling);
+        }, 2000);
+
+        $scope.$on('$destroy', function () {
+          $timeout.cancel(callTimer);
         });
+      }
 
+      startPulling();
 
-        $scope.loadData = function(groups, callback) {
+      $scope.unselectGroup = function (grpId) {
+        $scope.flatGroups.find(function (x) {
+          return x.id === grpId;
+        })['ticked'] = false;
+      }
 
-          if (typeof groups === 'undefined') {
-            $scope.groups = null;
-            return;
-          }
-
-          var groupsIds = groups.map(function (x) {
-            return x.id;
-          });
-
-          if (typeof groupsIds === 'undefined' || groupsIds.length == 0) {
-            $scope.groups = null;
-            return;
-          }
-
-            $resource('/quickview/group/:id').query({ id: groupsIds },
-                function(result) {
-                    $scope.groups = result;
-
-                    if(typeof callback == typeof Function) {
-                      callback();
-                    }
-                });
-        }
-
-      var startPulling = function() {
-          var callTimer = $timeout(function() {
-            $scope.loadData($scope.groupSelections, startPulling);
-          }, 2000);
-
-          $scope.$on('$destroy', function() {
-            $timeout.cancel(callTimer);
-          });
-        }
-
-        startPulling();
-
-        $scope.unselectGroup = function (grpId) {
-          $scope.flatGroups.find(function (x) {
-            return x.id === grpId;
-          })['ticked'] = false;
-        }
-
-      }]);
+    }]);
 
